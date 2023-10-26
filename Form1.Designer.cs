521d5ece340915639148efe9c0df360ed249333e
Luis Antonio Vasquez Sanchez <yancibeatriz123@hotmail.com>
2023-10-26 17:22:13 -0600

    se elimino lo innecesario

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,25 +40,19 @@
             this.pbocultar = new System.Windows.Forms.PictureBox();
             this.pbmostrar = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbocultar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbmostrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // btncontinuar
             // 
             this.btncontinuar.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btncontinuar.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btncontinuar.Location = new System.Drawing.Point(257, 258);
-            this.btncontinuar.Margin = new System.Windows.Forms.Padding(4);
+            this.btncontinuar.Location = new System.Drawing.Point(193, 210);
             this.btncontinuar.Name = "btncontinuar";
-            this.btncontinuar.Size = new System.Drawing.Size(104, 38);
+            this.btncontinuar.Size = new System.Drawing.Size(78, 31);
             this.btncontinuar.TabIndex = 7;
             this.btncontinuar.Text = "Continuar";
             this.btncontinuar.UseVisualStyleBackColor = true;
@@ -68,10 +62,9 @@
             // 
             this.btnlimpiar.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnlimpiar.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnlimpiar.Location = new System.Drawing.Point(148, 258);
-            this.btnlimpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnlimpiar.Location = new System.Drawing.Point(111, 210);
             this.btnlimpiar.Name = "btnlimpiar";
-            this.btnlimpiar.Size = new System.Drawing.Size(104, 37);
+            this.btnlimpiar.Size = new System.Drawing.Size(78, 30);
             this.btnlimpiar.TabIndex = 6;
             this.btnlimpiar.Text = "Eliminar";
             this.btnlimpiar.UseVisualStyleBackColor = true;
@@ -81,10 +74,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(172, 59);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(129, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(179, 32);
+            this.label1.Size = new System.Drawing.Size(142, 24);
             this.label1.TabIndex = 26;
             this.label1.Text = "Registrarse";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -92,23 +84,21 @@
             // tbxcontra
             // 
             this.tbxcontra.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.tbxcontra.Location = new System.Drawing.Point(175, 181);
-            this.tbxcontra.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxcontra.Location = new System.Drawing.Point(131, 147);
             this.tbxcontra.Multiline = true;
             this.tbxcontra.Name = "tbxcontra";
             this.tbxcontra.PasswordChar = '*';
-            this.tbxcontra.Size = new System.Drawing.Size(173, 32);
+            this.tbxcontra.Size = new System.Drawing.Size(131, 27);
             this.tbxcontra.TabIndex = 23;
             this.tbxcontra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxcontra_KeyPress);
             // 
             // Tbxusuario
             // 
             this.Tbxusuario.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.Tbxusuario.Location = new System.Drawing.Point(172, 130);
-            this.Tbxusuario.Margin = new System.Windows.Forms.Padding(4);
+            this.Tbxusuario.Location = new System.Drawing.Point(129, 106);
             this.Tbxusuario.Multiline = true;
             this.Tbxusuario.Name = "Tbxusuario";
-            this.Tbxusuario.Size = new System.Drawing.Size(221, 32);
+            this.Tbxusuario.Size = new System.Drawing.Size(167, 27);
             this.Tbxusuario.TabIndex = 22;
             this.Tbxusuario.TextChanged += new System.EventHandler(this.Tbxusuario_TextChanged);
             this.Tbxusuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Tbxusuario_KeyPress);
@@ -118,10 +108,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label3.Location = new System.Drawing.Point(48, 140);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(36, 114);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 23);
+            this.label3.Size = new System.Drawing.Size(72, 19);
             this.label3.TabIndex = 21;
             this.label3.Text = "Usuario";
             // 
@@ -129,19 +118,17 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(35, 191);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(26, 155);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 23);
+            this.label2.Size = new System.Drawing.Size(99, 19);
             this.label2.TabIndex = 20;
             this.label2.Text = "Contraseña";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(171, 304);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(128, 247);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 28);
+            this.button1.Size = new System.Drawing.Size(134, 23);
             this.button1.TabIndex = 29;
             this.button1.Text = "Crea una cuenta!!";
             this.button1.UseVisualStyleBackColor = true;
@@ -150,9 +137,8 @@
             // 
             this.pictureBox2.Image = global::Ejecicios_del_Gym.Properties.Resources.Captura_de_pantalla_2023_10_26_150707;
             this.pictureBox2.Location = new System.Drawing.Point(0, -2);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(105, 74);
+            this.pictureBox2.Size = new System.Drawing.Size(79, 60);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 28;
             this.pictureBox2.TabStop = false;
@@ -160,10 +146,9 @@
             // pbocultar
             // 
             this.pbocultar.Image = global::Ejecicios_del_Gym.Properties.Resources.Captura_de_pantalla_2023_09_08_211422;
-            this.pbocultar.Location = new System.Drawing.Point(353, 177);
-            this.pbocultar.Margin = new System.Windows.Forms.Padding(4);
+            this.pbocultar.Location = new System.Drawing.Point(265, 144);
             this.pbocultar.Name = "pbocultar";
-            this.pbocultar.Size = new System.Drawing.Size(40, 37);
+            this.pbocultar.Size = new System.Drawing.Size(30, 30);
             this.pbocultar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbocultar.TabIndex = 25;
             this.pbocultar.TabStop = false;
@@ -172,10 +157,9 @@
             // pbmostrar
             // 
             this.pbmostrar.Image = global::Ejecicios_del_Gym.Properties.Resources.Captura_de_pantalla_2023_09_08_211706;
-            this.pbmostrar.Location = new System.Drawing.Point(353, 177);
-            this.pbmostrar.Margin = new System.Windows.Forms.Padding(4);
+            this.pbmostrar.Location = new System.Drawing.Point(265, 144);
             this.pbmostrar.Name = "pbmostrar";
-            this.pbmostrar.Size = new System.Drawing.Size(40, 37);
+            this.pbmostrar.Size = new System.Drawing.Size(30, 30);
             this.pbmostrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbmostrar.TabIndex = 24;
             this.pbmostrar.TabStop = false;
@@ -185,58 +169,17 @@
             // 
             this.pictureBox1.Image = global::Ejecicios_del_Gym.Properties.Resources.Captura_de_pantalla_2023_09_20_112434;
             this.pictureBox1.Location = new System.Drawing.Point(0, -2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(525, 427);
+            this.pictureBox1.Size = new System.Drawing.Size(394, 347);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 27;
             this.pictureBox1.TabStop = false;
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(455, 292);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
-            this.label4.TabIndex = 30;
-            this.label4.Text = "label4";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(440, 67);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 31;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(440, 153);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(55, 50);
-            this.pictureBox3.TabIndex = 32;
-            this.pictureBox3.TabStop = false;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(424, 234);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 33;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // formlogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 423);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(394, 344);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label1);
@@ -249,14 +192,12 @@
             this.Controls.Add(this.btnlimpiar);
             this.Controls.Add(this.pbocultar);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "formlogin";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbocultar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbmostrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -276,10 +217,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Button button3;
     }
 }
 
